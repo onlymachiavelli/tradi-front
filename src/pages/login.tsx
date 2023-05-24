731d1517ed7ee77c17b5ff324c5f04ecc07e0407
Alaa Barka <tmakaveli643@gmail.com>
2023-05-24 01:06:20 +0100

    configuring routes and UI TWCSS

---
+++ src/pages/login.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react'
+
+const Login = () =>{
+    return (
+        <div className='w-full bg-white h-screen'>
+           <p className='bg-red'>hello</p> hello world
+        </div>
+    )
+
+}
+export default Login
---
+++ tailwind.config.js
@@ -0,0 +1,35 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+ 
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+
+  theme: {
+
+    screens:{
+      loginTablet : "927px"
+    },
+    extend: {
+      fontFamily: {
+        sans: ["Quicksand", "sans-serif"],
+
+      },
+
+      colors: {
+          dark : "#1e1e20",
+          purple : "#962134"
+      },
+      backgroundImage:{
+      }
+      
+    },
+
+    
+  },
+  plugins: [],
+}
