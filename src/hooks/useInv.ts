5afcac8c61b6d8aee60f69e61a0b1bc413283186
Alaa Barka <tmakaveli643@gmail.com>
2023-05-25 02:51:26 +0100

    connecting to firebase

--- src/hooks/useInv.ts
+++ src/hooks/useInv.ts
@@ -6,6 +6,19 @@ const useInv =() =>{
 
     const [cat, setCat] = React.useState()
     const [prod, setProd] = React.useState()
+
+
+
+
+
+    //to save a category
+    const [title, setTitle] = React.useState("")
+    const [description, setDescription] = React.useState("")
+    const [image, setImage] = React.useState("")
+
+    const saveCat = async ()=>{
+        
+    }
 }
 
 export default useInv
